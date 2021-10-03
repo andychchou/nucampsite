3da86fa8992f69a7a863f9054d06447877d3611f
Andy Chou <andychchou@gmail.com>
2021-10-02 22:59:56 -0700

    Icons

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Main from './components/MainComponent';
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs(['interpolate', 'It appears that you are using old', 'VirtualizedLists should never']);
 
 export default function App() {
   return (
--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import { Text, ScrollView } from 'react-native';
-import { Card } from 'react-native-elements';
-import { FlatList } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import { Text, ScrollView, FlatList } from 'react-native';
+import { Card, ListItem } from 'react-native-elements';
 import { PARTNERS } from '../shared/partners'
 
 function Mission() {
