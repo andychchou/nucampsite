1a79e6fbecd4517cc24103537e4bf2c050ac6235
Andy Chou <andychchou@gmail.com>
2021-10-08 20:20:47 -0700

    Using Redux

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Main from './components/MainComponent';
 import { LogBox } from 'react-native';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 LogBox.ignoreLogs(['interpolate', 'It appears that you are using old', 'VirtualizedLists should never']);
 
 export default function App() {
   return (
-    <Main />
+    <Provider store={store}>
+      <Main />
+    </Provider>
   );
 }
