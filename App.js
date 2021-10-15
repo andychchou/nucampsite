bac0541c113e68eec2f0c14b34c11b9d446dbe43
Andy Chou <andychchou@gmail.com>
2021-10-14 21:59:51 -0700

    Persist Redux Store

--- App.js
+++ App.js
@@ -3,15 +3,22 @@ import Main from './components/MainComponent';
 import { LogBox } from 'react-native';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
+import { PersistGate } from 'redux-persist/integration/react';
+import Loading from './components/LoadingComponent';
 
-const store = ConfigureStore();
+const { persistor, store } = ConfigureStore();
 
 LogBox.ignoreLogs(['interpolate', 'It appears that you are using old', 'VirtualizedLists should never']);
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Main />
+      <PersistGate
+        loading={<Loading />}
+        persistor={persistor}
+      >
+        <Main />
+      </PersistGate>
     </Provider>
   );
 }
